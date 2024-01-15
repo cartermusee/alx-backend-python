c4ec509b545a8d4fa4401edec197ba04317c2a0f
cartermusee <cartermusee@gmail.com>
2024-01-15 14:00:57 +0300

    async and await in python

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,7 @@ import random
 async def wait_random(max_delay=0):
     """function of asyn basic
     Keyword arguments:
-    max_delay: int arg with a default value 10
+        max_delay: int arg with a default value 10
     Returns: random delay
     """
     ran_delay = random.uniform(0, max_delay)
