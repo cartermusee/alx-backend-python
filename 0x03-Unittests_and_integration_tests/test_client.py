3b29c5da10901c0d2c6a39c29d61bb27de6d089e
cartermusee <cartermusee@gmail.com>
2024-02-04 22:09:06 +0300

    utils.access_nested_map_exception test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """modeule for client"""
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 from typing import Dict
@@ -14,9 +14,22 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc", {'login': "abc"})
     ])
     @patch("utils.get_json")
-    def test_org(self, org_name: str, expected_url: Dict, mock_get_json: MagicMock) -> None:
+    def test_org(self, org_name: str,
+                 expected_url: Dict,
+                 mock_get_json: MagicMock) -> None:
         """Test GithubOrgClient.org returns the correct value."""
         client = GithubOrgClient(org_name)
         client.org
         mock_get_json.assert_called_once_with(expected_url)
         self.assertFalse(mock_get_json.called)
+
+    @patch("client.GithubOrgClient.org", new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org) -> None:
+        """Test GithubOrgClient._public_repos_url."""
+        expected_payload = {
+            "repos_url": "https://api.github.com/orgs/google/repos"
+        }
+        mock_org.return_value = expected_payload
+        client = GithubOrgClient("google")
+        result = client._public_repos_url
+        self.assertEqual(result, expected_payload["repos_url"])
