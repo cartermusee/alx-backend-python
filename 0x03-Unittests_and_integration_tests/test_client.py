04027964be978363b8b1b302a1f5df847f14e836
cartermusee <cartermusee@gmail.com>
2024-02-04 22:01:02 +0300

    utils.access_nested_map_exception test

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""modeule for client"""
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """TestGithubOrgClient class"""
+    @parameterized.expand([
+        ("google", "https://api.github.com/orgs/google"),
+        ("abc", "https://api.github.com/orgs/abc")
+    ])
+    @patch("utils.get_json")
+    def test_org(self, org_name, expected_url, mock_get_json):
+        """Test GithubOrgClient.org returns the correct value."""
+        client = GithubOrgClient(org_name)
+        client.org
+        mock_get_json.assert_called_once_with(expected_url)
+        self.assertFalse(mock_get_json.called)
